f12e30acade8b9f93551d4ba8383483f1454d3dc
CheatCodeRevolution <julfikurrahman24@gmail.com>
2025-09-21 21:10:02 +0530

    Update _GenerateKey.lua

--- _GenerateKey.lua
+++ _GenerateKey.lua
@@ -3,10 +3,10 @@ local file = io.open("/sdcard/fvc", "w")
 if file then
     file:write(key)
     file:close()
-    gg.alert("Key generate Successfully..\nThis Is One Time Set-up Only...\nNo need to Run This Script Again.")
+    gg.alert("Key generate Successfully..\nThis Is One Time Set-up Only...\nNo need to Run This Script Again.\n"..key)
 else
     gg.alert(" Unable To Generate Key......")
     os.exit()
 end
 
-print("❤️ Thank You ❤️")
+print("❤️ Thank You ❤️\n"..key)
